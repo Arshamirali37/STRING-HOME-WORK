db1095cefbf9ca5196c74dbd2f6b1ed9bcb8c19e
ARSHamirali <arshamirali604@gmail.com>
2024-10-03 11:22:21 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,95 @@
+
+
+// Q1
+// var firstname=prompt('ENTER YOUR FIRST NAME..')
+// var LASTname=prompt('ENTER YOUR LAST NAME..')
+// console.log(firstname , LASTname)
+
+// Q2
+// var mobile=prompt("Enter your favourite Smart phone")
+// console.log(`My Favourite phone is ${mobile}`)
+// console.log(mobile.length )
+
+// Q3
+// var country="Pakistan"
+// var n = country.indexOf("n")
+// console.log(n)
+
+// Q4
+// var greeting="Hello World"
+//  var l = country.indexOf("l")
+// console.log(l)  
+
+// Q5
+// var country="Pakistan"
+// var n = country.slice(3,4)
+// console.log(n)
+
+// Q6
+// var firstname=prompt('ENTER YOUR FIRST NAME..')
+// var LASTname=prompt('ENTER YOUR LAST NAME..')
+// console.log(firstname , LASTname)
+
+// var city='Hyderabad'
+// var replacement='Islam'
+// var slice=city.slice(5,9)
+// console.log(replacement+slice)
+
+// Q8
+// var message = "Ali and Sami are best friends. They play cricket and football together";
+// var indexnum=message.indexOf("and")
+// // console.log(indexnum)
+// var starting =message.slice(0,4)
+// var replacement="&"
+// var remaintext=message.slice(indexnum+12)
+// console.log(starting+replacement+remaintext)
+ 
+// {Method #2}
+// var messagenew=message.replaceAll("and","&")
+// console.log(messagenew)
+
+// Q9
+// var userinput=prompt("Ente any word")
+// var USERINPUT=userinput.toUpperCase()
+// console.log(USERINPUT)
+// console.log(userinput)
+
+// Q10
+// var userinput=prompt("Ente any word")
+// var USERINPUT=userinput.()
+// console.log(USERINPUT)
+// console.log(userinput)
+
+// Q11
+// var number="427"
+// var numberinterger=Number(number)
+// console.log(numberinterger)
+
+// Q12
+// var num=35.36
+// var string1=num.toString()
+// console.log(string1)
+
+// Q13
+// var userinput=prompt("Enter your name")
+// var symbol=["!","@",".",","]
+// if(userinput === symbol[0]){
+// alert('Enter proper name')
+// }
+// else{
+//     alert("Proper name!")
+// }
+
+// PROBLEM IN QUESTION 14 & 15
+
+// Q16
+// var unversity="Universtiy of Karachi"
+// var myarray=unversity.split("")
+// console.log(myarray)
+
+// Q17
+// var country="Pakistan"
+// var n = country.indexOf("n")
+// // console.log(n)
+// var lastword=userinput.charAt(7)
+// console.log(lastword)
